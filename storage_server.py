ec6eeefe3f0fbcce408f73fe76e857a4ba787c07
JottoWorol <43534535+JottoWorol@users.noreply.github.com>
2020-10-13 22:24:11 +0300

    Update storage_server.py

--- storage_server.py
+++ storage_server.py
@@ -11,8 +11,58 @@ clients = []
 files = {}
 lastfilename = ""
 
-sibling_ips = []
-
+class Replicator:
+    def __init__(self):
+        self.isPrimary = false
+    
+    def SetSiblings(self, siblings):
+        self.isPrimary = true
+        self.sibling_ips = siblings
+        self.sockets = []
+        for ip in siblings:
+            s = socket.socket()
+            s.connect((ip, 5000))
+            self.sockets.append(s)
+            
+     def Replicate(self, name):
+        if not self.isPrimary:
+            return
+        for sock in self.sockets:
+            self.SendCommand('sendfile '+name)
+            self.SendFile(sock, name)
+            
+     def ReplicateEmpty(self, name):
+        if not self.isPrimary:
+            return
+        for sock in self.sockets:
+            self.SendCommand('sendfilempty '+name)
+            
+     def Remove(self, name):
+        if not self.isPrimary:
+            return
+        for sock in self.sockets:
+            self.SendCommand('remove '+name)
+            
+     def SendFile(self, sock, name):
+        bytes_to_send = open(name,'rb').read()
+        count = len(bytes_to_send)//1024
+        rem = len(bytes_to_send)%1024
+        if rem >0:
+            count+=1
+        sock.send(count.to_bytes(4,byteorder="big"))
+        sock.send(rem.to_bytes(4,byteorder="big"))
+        for i in range(count):
+            sock.send(bytes_to_send[1024*i:1024*(i+1)])
+      
+     def SendCommand(self, sock, command):
+        bytes_to_send = command.encode()
+        sock.send((len(bytes_to_send)).to_bytes(4,byteorder="big"))
+        sock.send(bytes_to_send)
+            
+        
+    
+replicator = Replicator()
+    
 # Thread to listen one particular client
 class ClientListener(Thread):
     def __init__(self, name: str, sock: socket.socket):
@@ -73,19 +123,22 @@ class ClientListener(Thread):
             command = self.ReadData()
             command = command.split()
             if command[0]=='siblings':
-                sibling_ips = command[1:]
+                replicator.SetSiblings(command[1:])
 
             if command[0]=='getfile':
                 self.SendFile(command[1])
 
             if command[0]=='sendfile':
                 open(command[1],'wb').write(self.ReadBytes())
+                replicator.Replicate(command[1])
 
             if command[0]=='sendfilempty':
                 open(command[1],'a').close()
+                replicator.ReplicateEmpty(command[1])
 
             if command[0]=='remove':
                 os.remove(command[1])
+                replicator.Remove(command[1])
 
             if command[0]=='size':
                 self.SendData(str(os.path.getsize(command[1])))
