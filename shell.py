cbd20b724717b102682a4d3affc96adde84e25a3
dzekegor <gubanov.egor2010@yandex.ru>
2020-10-13 20:17:35 +0300

    Client final variant

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import os
 import socket
 
-host = ""
-port = 22
+host = "104.197.214.149"
+port = 8080
 
 addr = (host, port)
 sock = socket.socket()
@@ -36,8 +36,14 @@ def lexer(c):
 cur_dir = "/"
     
 def absolutize(arg):
+    if arg == "":
+        return cur_dir
+    if arg == "..":
+        if "/".join(cur_dir.split("/")[:-2]) == "":
+            return "/"
+        return "/".join(cur_dir.split("/")[:-2])
     if arg[0:2] == "..":
-        arg = "/".join(cur_dir.split("/")[::-2]) + arg[2:]
+        arg = "/".join(cur_dir.split("/")[:-2]) + arg[2:]
     if arg[0] == ".":
         arg = cur_dir + arg[2:]
     if arg[0] != "/":
@@ -46,12 +52,14 @@ def absolutize(arg):
         arg = arg[:-1]
     return arg
 def shell(lex,arg):
+    global cur_dir
     if lex=='exit': 
         return True
     elif lex=='cd': # Open directory
         if arg != "":
             res = send(lex+' '+arg)
             if res != "Error":
+                
                 if arg[-1] != "/":
                     arg = arg + "/"
                 cur_dir = arg
@@ -73,14 +81,42 @@ def shell(lex,arg):
         res = send(lex)
         print(res)
     elif lex=='cat': # Read file
-        res = send(lex+' '+arg)
+        msg = lex+' '+arg
+        sock.send((len(msg)).to_bytes(4,byteorder="big"))
+        sock.send(bytes(msg.encode("utf-8")))
+        count = int.from_bytes(sock.recv(4), "big")
+        last_size = int.from_bytes(sock.recv(4), "big")
+        result = b''
+        for i in range(count):
+            if i==count-1:
+                if last_size==0:
+                    result+=sock.recv(1024)
+                else:
+                    result+=sock.recv(last_size)
+            else:
+                result+=sock.recv(1024)
+        print(result.decode("utf-8"))
+        
     elif lex=='send': # Send file
         arg = arg.split(" ")
         f = open(arg[0],"rb").read()
         res = send(lex+' '+arg[1])
         print(res)
-        res = send(f)
-        print(res)
+        p_number = len(f)//1024
+        if len(f) % 1024 > 0:
+            p_number += 1
+        
+        sock.send((p_number).to_bytes(4,byteorder="big"))
+        if len(f) % 1024 != 0:
+            sock.send((len(f) % 1024).to_bytes(4,byteorder="big"))
+        else:
+            sock.send((1024).to_bytes(4,byteorder="big"))
+        f = open(arg[0],"rb")
+        for i in range(p_number):
+            sock.send(f.read(1024))
+        size = int.from_bytes(sock.recv(4),byteorder="big")
+        data = sock.recv(size)
+        print(data.decode("utf-8"))
     elif lex=="cp": # Copy file
         res = send(lex+' '+arg)
         print(res)
